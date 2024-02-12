5d47139d3471e994cc95c7129b10d8ddf42f42c7
Alexander_Shevchenko_ <andreyjakovlev229@gmail.com>
2024-02-12 10:51:30 +0300

    initial

---
+++ src/components/Tree.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="service in services" :key="service.id">
+        {{ service.name }} ({{ service.price }})
+        <template v-if="service.node === 1">
+          <tree :services="getChildren(service.id)" />
+        </template>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tree',
+  props: {
+    services: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    getChildren(parentId) {
+      return this.services.filter(service => service.head === parentId);
+    }
+  }
+};
+</script>
